4a830835d38d66728ab328beb175e399c0e4c57e
Samuel William Noah <noah.samuelwilliam@gmail.com>
2022-07-14 16:48:03 +0700

    Styling the home component and the navbar

--- src/Home.js
+++ src/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
             </div>
             <div className="home-right">
                 <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>
-                <Link to={"/sign-up"}>Sign Up</Link>
+                <button className="sign-up-button"><Link style = {{textDecoration: 'none'}}to={"/sign-up"}>Sign Up</Link></button>
             </div>
         </div>
      );
